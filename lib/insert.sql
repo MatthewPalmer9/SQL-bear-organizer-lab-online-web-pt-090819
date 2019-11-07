bd804bc67db6b9335bd00fc0567ecc42009ce721
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-11-07 20:18:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Ch
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "Male", "Black", "Calm", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "Male", "Black", "Calm", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 5, "Male", "Black", "Calm", "true");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Black", "Calm", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Male", "Black", "Calm", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Black", "Calm", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Black", "Calm", "true");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "Male", "Black", "Calm", "true");
